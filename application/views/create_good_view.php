d0a22139b31ed94fbff2b41d7359a826e5d23b45
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-05-04 15:59:14 +0300

    Отображается в случаи успешного создания статьи

---
+++ application/views/create_good_view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <meta http-equiv = "content-type" content = "text/html; charset=utf-8">
+  <title>Создание задачи</title>
+</head>
+<body>
+  <h4>
+    Задача успешно создана!
+  </h4>
+  <a href="/main/sort_by_name">
+    Перейти к списку задач
+  </a>
+</body>
+</html>
