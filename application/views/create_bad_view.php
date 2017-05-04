f338791270efda4286ceb79edd543168d5a79e63
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-05-04 15:57:45 +0300

    В случаи ошибки создания статьи

---
+++ application/views/create_bad_view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <meta http-equiv = "content-type" content = "text/html; charset=utf-8">
+  <title>Создание задачи</title>
+</head>
+<body>
+  <h4>
+    Ошибка создания.
+  </h4>
+  <a href="/create">
+    Перейти к созданию задачи
+  </a>
+</body>
+</html>
