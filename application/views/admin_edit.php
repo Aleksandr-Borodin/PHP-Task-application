db9600006d69628e0b088b296290cedc63c8ae1b
Aleksandr-Borodin <aleksandrov477@gmail.com>
2018-07-21 23:55:26 +0300

    Добавлены мета данные

--- application/views/admin_edit.php
+++ application/views/admin_edit.php
@@ -3,7 +3,11 @@
 <head>
   <link rel = "stylesheet" href = "/css/main.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="shortcut icon" href="../../images/favicon.ico" type="image/x-icon">
+  <link rel="image_src" href="../../images/open-graph-tasks.jpg">
   <meta http-equiv = "content-type" content = "text/html; charset=utf-8">
+  <meta property="og:title" content="Tasks">
+  <meta property="og:image" content="/images/open-graph-tasks.jpg">
   <title>Изменение записи</title>
 </head>
 <body>
